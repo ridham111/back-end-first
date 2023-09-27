e1c017cae2e23aa70f3fa6634bb8ea135bef0a74
ridham111 <90979646+ridham111@users.noreply.github.com>
2023-09-27 22:36:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ app.get('/projects/:projectId', async (req, res) => {
 
 
 // Start the server
-const PORT = 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
